7cdd420a2a614cf190695f26490ffe085cda5d81
semih1239 <semihayyildiz23@hotmail.com>
2021-11-02 00:35:07 +0100

    Added Footer Part

---
+++ src/client/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './footer.css'
+
+export const Footer = () => {
+
+    return <footer>
+        <div className='footer'>
+            <div className="column">
+                <h3>Company</h3>
+                <ul>
+                    <li>About us</li>
+                    <li>FAQ</li>
+                    <li>Contact us</li>
+                    <li>Pricing</li>
+                </ul>
+            </div>
+            <div className="column">
+                <h3>Resources</h3>
+                <ul>
+                    <li>Help center</li>
+                    <li>Term of service</li>
+                    <li>Legal</li>
+                    <li>GDPR</li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+}
---
+++ src/client/components/Footer/Footer.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { Footer } from './Footer';
+
+
+export default { title: 'Components / Footer' };
+
+export const Component = () => <Footer />;
