d73e532c3d8652150b1b4fcf8450f7c4ce90885c
BikramRawat <mail2bikram44@gmail.com>
2021-11-04 17:05:32 +0100

    ProductsPagination Component Added

---
+++ src/client/components/ProductsPagination/ProductsPagination.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ProductsPagiantion from './ProductsPagination.component';
+
+export default { title: 'Products Pagination' };
+export const component = () => <ProductsPagiantion />;
