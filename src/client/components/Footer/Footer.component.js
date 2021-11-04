87fd5dc3c03202869d4e4efc737fe2e6707415a9
semih1239 <semihayyildiz23@hotmail.com>
2021-11-04 22:32:09 +0100

    static code fix

--- src/client/components/Footer/Footer.component.js
+++ src/client/components/Footer/Footer.component.js
@@ -6,19 +6,19 @@ export const Footer = () => {
         <div className="column">
             <h3>Company</h3>
             <ul>
-                <li><a>About us</a></li>
-                <li><a>FAQ</a></li>
-                <li><a>Contact us</a></li>
-                <li><a>Pricing</a></li>
+                <li><span>About us</span></li>
+                <li><span>FAQ</span></li>
+                <li><span>Contact us</span></li>
+                <li><span>Pricing</span></li>
             </ul>
         </div>
         <div className="column">
             <h3>Resources</h3>
             <ul>
-                <li><a>Help center</a></li>
-                <li><a>Term of service</a></li>
-                <li><a>Legal</a></li>
-                <li><a>GDPR</a></li>
+                <li><span>Help center</span></li>
+                <li><span>Term of service</span></li>
+                <li><span>Legal</span></li>
+                <li><span>GDPR</span></li>
             </ul>
         </div>
     </footer>
