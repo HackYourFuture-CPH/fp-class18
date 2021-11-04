214f51fbaa860e95190cc59abd33200d2e3095e6
semih1239 <semihayyildiz23@hotmail.com>
2021-11-04 18:56:10 +0100

    Updated codes with feedbacks

--- src/client/components/Footer/Footer.component.js
+++ src/client/components/Footer/Footer.component.js
@@ -2,27 +2,24 @@ import React from "react";
 import './Footer.styles.css'
 
 export const Footer = () => {
-
     return <footer>
-        <div className='footer'>
-            <div className="column">
-                <h3>Company</h3>
-                <ul>
-                    <li>About us</li>
-                    <li>FAQ</li>
-                    <li>Contact us</li>
-                    <li>Pricing</li>
-                </ul>
-            </div>
-            <div className="column">
-                <h3>Resources</h3>
-                <ul>
-                    <li>Help center</li>
-                    <li>Term of service</li>
-                    <li>Legal</li>
-                    <li>GDPR</li>
-                </ul>
-            </div>
+        <div className="column">
+            <h3>Company</h3>
+            <ul>
+                <li><a>About us</a></li>
+                <li><a>FAQ</a></li>
+                <li><a>Contact us</a></li>
+                <li><a>Pricing</a></li>
+            </ul>
+        </div>
+        <div className="column">
+            <h3>Resources</h3>
+            <ul>
+                <li><a>Help center</a></li>
+                <li><a>Term of service</a></li>
+                <li><a>Legal</a></li>
+                <li><a>GDPR</a></li>
+            </ul>
         </div>
     </footer>
 }
