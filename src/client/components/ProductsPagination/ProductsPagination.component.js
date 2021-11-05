f2a019bbdf8ac178423849567ef9e7564c59b135
BikramRawat <mail2bikram44@gmail.com>
2021-11-05 09:00:50 +0100

    Products Pagination Component Added

---
+++ src/client/components/ProductsPagination/ProductsPagination.component.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import productsImages from './ProductsPaginationImages.component';
+import './ProductsPagination.styles.css';
+
+const ProductsPagiantion = () => {
+  const productsPerPage = 3;
+  const [pageNumber, setPageNumber] = useState(0);
+  const pagesVisited = pageNumber * productsPerPage;
+
+  const displayProducts = productsImages
+    .slice(pagesVisited, pagesVisited + productsPerPage)
+    .map((image) => {
+      return (
+        <div>
+          <img src={image} alt="products-img" />
+        </div>
+      );
+    });
+
+  const pageCount = Math.ceil(productsImages.length / productsPerPage);
+
+  const handlePageClick = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  return (
+    <>
+      <h3>ALL PRODUCTS</h3>
+      <div className="displayImgs">
+        {displayProducts}
+        <ReactPaginate
+          previousLabel="Previous"
+          nextLabel="Next"
+          pageCount={pageCount}
+          onPageChange={handlePageClick}
+          containerClassName="paginationButtons"
+          previousLinkClassName="previousBttn"
+          nextLinkClassName="nextBttn"
+          disabledClassName="paginationDisabled"
+          activeClassName="paginationActive"
+        />
+      </div>
+    </>
+  );
+};
+
+export default ProductsPagiantion;
