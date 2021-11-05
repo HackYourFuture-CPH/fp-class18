d064301105faa2b8601ffec039c228f5ac3133ac
BikramRawat <mail2bikram44@gmail.com>
2021-11-05 09:55:12 +0100

    created migrations for products table incl. categories table

---
+++ src/server/migrations/20211105095000_products-table.js
@@ -0,0 +1,39 @@
+//migrations for products table, also contains categories table since products table have foreign -
+//key constraint with categories table
+
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('categories', (table) => {
+      table.increments('id').primary();
+      table.string('name').notNullable();
+      table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
+    })
+    .createTable('products', (table) => {
+      table.increments('id').primary();
+      table.string('name').notNullable();
+      table.decimal('price').notNullable();
+      table
+        .enum('color', ['red', 'green', 'blue', 'yellow', 'white', 'black'])
+        .notNullable();
+      table
+        .enum('size', ['small', 'medium', 'large', 'extra-large', 'doubleXL'])
+        .notNullable();
+      table
+        .enum('status', ['out_of_stock', 'in_stock', 'running_low'])
+        .notNullable();
+      table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
+      table.binary('picture').notNullable();
+      table.integer('stock_amount').notNullable();
+      table
+        .integer('category_id')
+        .unsigned()
+        .references('id')
+        .inTable('categories')
+        .notNullable()
+        .onDelete('CASCADE')
+        .index();
+    });
+};
+exports.down = function (knex) {
+  return knex.schema.dropTable('products').dropTable('categories');
+};
