4ee52cb75d76013aaa4dff791f06c35de92a3b37
varsha verma <varsha.verma85@gmail.com>
2021-11-05 12:33:15 +0100

    removing button component

--- src/client/components/heroImage/heroImageComponent/heroImage.js
+++ src/client/components/heroImage/heroImageComponent/heroImage.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import './heroImage.css';
 import heroImg from '../../../assets/images/hero-image.png';
-import Button from '../buttonComponent/button';
 import PropTypes from 'prop-types';
 
-function onClickHandler() {
-  // eslint-disable-next-line no-console
-  console.log('Sign up button is clicked');
-}
 export default function HeroImage({ heroText }) {
   return (
     <div className="hero-image">
       <img src={heroImg} alt="" />
       <div className="hero-text">
         <h3 className="title-text">{heroText}</h3>
-        <Button label="Sign Up" onClick={() => onClickHandler()} />
       </div>
     </div>
   );
