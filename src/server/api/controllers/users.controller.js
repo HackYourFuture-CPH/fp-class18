c7f316165d9d53f9800b842a5fa0266d23844cf9
semih1239 <semihayyildiz23@hotmail.com>
2021-11-06 02:47:14 +0100

    Backend: Endpoint to retrieve user info- GET api/users/{user_id}

---
+++ src/server/api/controllers/users.controller.js
@@ -0,0 +1,28 @@
+const knex = require('../../config/db');
+const HttpError = require('../lib/utils/http-error');
+const moment = require('moment-timezone');
+
+const getUsers = async () => {
+  return knex('users');
+};
+
+const getUsersById = async (id) => {
+  if (!id) {
+    throw new HttpError('Id should be a number', 400);
+  }
+
+  try {
+    const users = await knex('users').where({ id });
+    if (users.length === 0) {
+      throw new Error(`incorrect entry with the id of ${id}`, 404);
+    }
+    return users;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+module.exports = {
+  getUsers,
+  getUsersById,
+};
---
+++ src/server/api/routes/users.router.js
@@ -0,0 +1,62 @@
+const express = require('express');
+
+const router = express.Router({ mergeParams: true });
+
+// controllers
+const usersController = require('../controllers/users.controller');
+
+/**
+ * @swagger
+ * /modules:
+ *  get:
+ *    tags:
+ *    - Modules
+ *    summary: Get all modules
+ *    description:
+ *      Will return all modules.
+ *    produces: application/json
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.get('/', (req, res, next) => {
+  usersController
+    .getUsers()
+    .then((result) => res.json(result))
+    .catch(next);
+});
+
+/**
+ * @swagger
+ * /modules/{ID}:
+ *  get:
+ *    tags:
+ *    - Modules
+ *    summary: Get module by ID
+ *    description:
+ *      Will return single module with a matching ID.
+ *    produces: application/json
+ *    parameters:
+ *     - in: path
+ *       name: ID
+ *       schema:
+ *         type: integer
+ *         required: true
+ *         description: The ID of the module to get
+ *
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.get('/:id', (req, res, next) => {
+  usersController
+    .getUsersById(req.params.id)
+    .then((result) => res.json(result))
+    .catch(next);
+});
+
+module.exports = router;
