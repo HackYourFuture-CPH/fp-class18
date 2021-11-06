653914ce53e47b6a1d196c6ba235f82c7f0f42c0
zmzade <zmahmoodzadeh@yahoo.com>
2021-11-06 22:41:54 +0100

    add orders router

---
+++ src/server/api/routes/orders.router.js
@@ -0,0 +1,62 @@
+const express = require('express');
+
+const router = express.Router({ mergeParams: true });
+
+// controllers
+const ordersController = require('../controllers/orders.controller');
+
+/**
+ * @swagger
+ * /modules:
+ *  get:
+ *    tags:
+ *    - Modules
+ *    summary: Get all modules
+ *    description:
+ *      Will return all modules.
+ *    produces: application/json
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.get('/', (req, res, next) => {
+  ordersController
+    .getModules()
+    .then((result) => res.json(result))
+    .catch(next);
+});
+
+/**
+ * @swagger
+ * /modules/{ID}:
+ *  get:
+ *    tags:
+ *    - Modules
+ *    summary: Get module by ID
+ *    description:
+ *      Will return single module with a matching ID.
+ *    produces: application/json
+ *    parameters:
+ *     - in: path
+ *       name: ID
+ *       schema:
+ *         type: integer
+ *         required: true
+ *         description: The ID of the module to get
+ *
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.get('/:id', (req, res, next) => {
+  ordersController
+    .getOrderById(req.params.id)
+    .then((result) => res.json(result))
+    .catch(next);
+});
+
+module.exports = router;
