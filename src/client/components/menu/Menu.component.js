c1c8f0b8297a9940311df633b484a719e95c02ec
saru <saru.gmesp@gmail.com>
2021-11-07 18:45:01 +0100

    Feedback changes made

--- src/client/components/menu/Menu.component.js
+++ src/client/components/menu/Menu.component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import { Link } from 'react-router-native';
+
 import { BrowserRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './Menu.styles.css';
