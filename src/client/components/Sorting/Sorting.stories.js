75736ae3d7f65bc345e1514199d1a3cfb9316bae
zmzade <zmahmoodzadeh@yahoo.com>
2021-11-07 20:32:53 +0100

    update sorting.stories.js

--- src/client/components/Sorting/Sorting.stories.js
+++ src/client/components/Sorting/Sorting.stories.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import Sorting from './Sorting.component';
-import SortingCategories from './SortingCategories.component';
-import SortingOptions from './SortingOptions.component';
 
-export default { title: 'Some title' };
+export default { title: 'Components/Sorting' };
 
 export const sorting = () => <Sorting />;
-export const sortingCategories = () => <SortingCategories />;
-export const sortingOptions = () => <SortingOptions />;
