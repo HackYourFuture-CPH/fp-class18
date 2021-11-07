4533330d0498bbf020b686bc993c1f8239236312
semih1239 <semihayyildiz23@hotmail.com>
2021-11-08 00:55:57 +0100

    Databases: Create Seeding for Products and Categories Table Separately

--- src/server/seeds/development/02_products.js
+++ src/server/seeds/development/02_products.js
@@ -1,33 +1,7 @@
 exports.seed = function (knex) {
-  return knex('categories')
+  return knex('products')
     .del()
-    .then(() => {
-      return knex('products').del();
-    })
-    .then(() => {
-      return knex('categories').insert([
-        {
-          id: 1,
-          name: 'Furniture',
-          created_at: '2020-05-11 00:00:00',
-        },
-        {
-          id: 2,
-          name: 'Lamps',
-          created_at: '2020-05-11 00:00:00',
-        },
-        {
-          id: 3,
-          name: 'Home decor',
-          created_at: '2020-05-11 00:00:00',
-        },
-        {
-          name: 'Linens',
-          created_at: '2020-05-11 00:00:00',
-        },
-      ]);
-    })
-    .then(() => {
+    .then(function () {
       return knex('products').insert([
         {
           name: 'Ceramic pot',
