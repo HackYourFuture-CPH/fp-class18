3bd04e8b89d3c35a8fcd66bb589d59180f0f455a
Olena.Kasiianenko <olena.kasiianenko@gmail.com>
2021-11-07 13:05:04 +0100

    Changed name for merger

---
+++ src/server/migrations/20211108150400_order_items.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('order_items', (table) => {
+    table.integer('id').primary();
+    table.integer('order_id').notNullable().references('id').inTable('orders');
+    table
+      .integer('product_id')
+      .notNullable()
+      .references('id')
+      .inTable('products');
+    table.integer('quantity').defaultTo(1).notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('order_items');
+};
