3e8d18ff52299036672eea475878e22943d39678
simonreddy2001 <simon.reddy2001@gmail.com>
2021-11-10 14:56:48 +0100

    import file name

--- src/client/components/Button/Button.stories.js
+++ src/client/components/Button/Button.stories.js
@@ -2,7 +2,7 @@ import { text } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import React from 'react';
 
-import ButtonComponent from './ButtonComponent';
+import ButtonComponent from './Button.component';
 
 export default {
   title: 'Components / Button Component',
