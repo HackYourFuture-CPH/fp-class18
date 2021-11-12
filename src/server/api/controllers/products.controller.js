6e9975ceb385521c5bdca0dcef9960e91962ea38
Olena.Kasiianenko <olena.kasiianenko@gmail.com>
2021-11-12 18:30:14 +0100

    Clean code

--- src/server/api/controllers/products.controller.js
+++ src/server/api/controllers/products.controller.js
@@ -10,7 +10,7 @@ const getProducts = async (request) => {
       .select('products.*')
       .where('categories.name', 'like', `%${request.query.category}%`);
   }
-  return await products;
+  return products;
 };
 
 const getProductById = async (id) => {
