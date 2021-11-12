e711da6a28e9cc1950b79b42047ee1168a9715dd
zmzade <zmahmoodzadeh@yahoo.com>
2021-11-12 11:02:54 +0100

    added api/categories

---
+++ src/server/api/routes/categories.router.js
@@ -0,0 +1,60 @@
+const express = require('express');
+
+const router = express.Router({ mergeParams: true });
+
+// controllers
+const categoriesController = require('../controllers/categories.controller');
+
+/**
+ * @swagger
+ * /categories:
+ *  get:
+ *    tags:
+ *    - Categories
+ *    summary: Get all categories
+ *    description:
+ *      Will return all categories.
+ *    produces: application/json
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.get('/', (req, res, next) => {
+  categoriesController
+    .getCategories()
+    .then((result) => res.json(result))
+    .catch(next);
+});
+
+/**
+ * @swagger
+ * /categories/{ID}:
+ *  get:
+ *    tags:
+ *    - Categories
+ *    summary: Get module by ID
+ *    description:
+ *      Will return single module with a matching ID.
+ *    produces: application/json
+ *    parameters:
+ *     - in: path
+ *       name: ID
+ *       schema:
+ *         type: integer
+ *         required: true
+ *         description: The ID of the module to get
+ *
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.get('/:id', (req, res, next) => {
+  categoriesController
+    .getCategoriesById(req.params.id)
+    .then((result) => res.json(result))
+    .catch(next);
+});
