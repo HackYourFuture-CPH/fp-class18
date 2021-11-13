4c1db73026f7a32ee5e8f347e352de1aeab77541
semih1239 <semihayyildiz23@hotmail.com>
2021-11-14 00:57:21 +0100

    Backend: Endpoint to update delivery info for user - PUT api/users/{user_id}

--- src/server/api/controllers/users.controller.js
+++ src/server/api/controllers/users.controller.js
@@ -7,13 +7,19 @@ const getUsers = async () => {
 
 const getUsersById = async (id) => {
   if (!id) {
-    throw new HttpError('Bad request. User ID must be an integer and larger than 0', 400);
+    throw new HttpError(
+      'Bad request. User ID must be an integer and larger than 0',
+      400,
+    );
   }
 
   try {
     const users = await knex('users').where({ id });
     if (users.length === 0) {
-      throw new Error(`A user with the specified ID was not found : ${id}`, 404);
+      throw new Error(
+        `A user with the specified ID was not found : ${id}`,
+        404,
+      );
     }
     return users;
   } catch (error) {
@@ -21,8 +27,20 @@ const getUsersById = async (id) => {
   }
 };
 
+const editUser = async (UserId, updatedUser) => {
+  if (!parseInt(UserId)) {
+    throw new HttpError('UserId should be a number', 400);
+  }
+  return await knex('users').where({ id: UserId }).update({
+    address: updatedUser.address,
+    city: updatedUser.city,
+    zipcode: updatedUser.zipcode,
+    country: updatedUser.country,
+  });
+};
+
 module.exports = {
   getUsers,
   getUsersById,
+  editUser,
 };
-
