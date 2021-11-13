46f018e04f0a0f72772f6b2af10a7b78bf385fc9
Bikram Rawat <mail2bikram44@gmail.com>
2021-11-13 22:45:12 +0100

    Fixed issues with props for Products Pagination, & improved CSS

--- src/client/components/ProductsPagination/ProductsPagination.component.js
+++ src/client/components/ProductsPagination/ProductsPagination.component.js
@@ -2,29 +2,46 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './ProductsPagination.styles.css';
 
-const ProductsPagination = () => {
-  const productsPerPage = 3;
-
+const ProductsPagination = ({
+  arrayToFilter,
+  productsPerPage,
+  onPageChange,
+}) => {
   const [currentPage, setCurrentPage] = useState(1);
-  // i have hard coded the products array length and the productsPerPage, since i will get these as props
-  // from another component which is not yet merged
-  const productsLen = 37;
   const pageNumberLimit = 5;
   const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
   const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
 
   const pages = [];
-  for (let i = 1; i <= Math.ceil(productsLen / productsPerPage); i += 1) {
+  for (
+    let i = 1;
+    i <= Math.ceil(arrayToFilter.length / productsPerPage);
+    i += 1
+  ) {
     pages.push(i);
   }
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const getCurrentProducts = (page) => {
+    const indexOfLastProduct = page * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProductsOnPage = arrayToFilter.slice(
+      indexOfFirstProduct,
+      indexOfLastProduct,
+    );
+    return currentProductsOnPage;
+  };
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    return getCurrentProducts(pageNumber);
+  };
 
   const displayPageNumbers = pages.map((number) => {
     if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
       return (
         <li
           key={number}
-          onClick={() => paginate(number)}
+          onClick={() => onPageChange(paginate(number))}
           className={currentPage === number ? 'active' : null}
         >
           {number}
@@ -40,6 +57,7 @@ const ProductsPagination = () => {
       setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
       setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
     }
+    return getCurrentProducts(currentPage + 1);
   };
 
   const handlePrevBtn = () => {
@@ -48,15 +66,20 @@ const ProductsPagination = () => {
       setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
       setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
     }
+    return getCurrentProducts(currentPage - 1);
   };
 
   let pageIncrementBtn = null;
   if (pages.length > maxPageNumberLimit) {
-    pageIncrementBtn = <li onClick={handleNextBtn}> &hellip;</li>;
+    pageIncrementBtn = (
+      <li onClick={() => onPageChange(handleNextBtn())}> &hellip;</li>
+    );
   }
   let pageDecrementBtn = null;
   if (minPageNumberLimit >= 1) {
-    pageDecrementBtn = <li onClick={handlePrevBtn}> &hellip;</li>;
+    pageDecrementBtn = (
+      <li onClick={() => onPageChange(handlePrevBtn())}> &hellip;</li>
+    );
   }
 
   return (
@@ -65,8 +88,8 @@ const ProductsPagination = () => {
         <li>
           <button
             type="button"
-            onClick={handlePrevBtn}
-            disabled={currentPage === pages[0] ? true : false}
+            onClick={() => onPageChange(handlePrevBtn())}
+            disabled={currentPage === 1}
           >
             &#10094; Prev
           </button>
@@ -77,8 +100,8 @@ const ProductsPagination = () => {
         <li>
           <button
             type="button"
-            onClick={handleNextBtn}
-            disabled={currentPage === pages[pages.length - 1] ? true : false}
+            onClick={() => onPageChange(handleNextBtn())}
+            disabled={currentPage === pages.length}
           >
             Next &#10095;
           </button>
@@ -89,12 +112,13 @@ const ProductsPagination = () => {
 };
 
 ProductsPagination.propTypes = {
-  products: PropTypes.arrayOf,
+  arrayToFilter: PropTypes.array,
   productsPerPage: PropTypes.number,
   onPageChange: PropTypes.func,
 };
 
 ProductsPagination.defaultProps = {
-  productsImages: [],
+  arrayToFilter: [],
+  productsPerPage: 6,
 };
 export default ProductsPagination;
