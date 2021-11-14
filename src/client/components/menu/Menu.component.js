c405bc6258f05929e92c1d6cae612e06dc9434d8
saru <saru.gmesp@gmail.com>
2021-11-14 12:22:40 +0000

    Feedback changes

--- src/client/components/menu/Menu.component.js
+++ src/client/components/menu/Menu.component.js
@@ -7,14 +7,13 @@ import faShoppingCart from '../../assets/images/shopping-cart.png';
 
 export const Menu = () => {
   return (
-    <menu>
-      <header>
-        <BrowserRouter>
+    <BrowserRouter>
+      <menu>
+        <header>
           <div className="logo_container">
             <div className="user-icon">
               <div className="dropdown">
                 <img src={faUser} alt="login" />
-
                 <div id="login" className="dropdown-content">
                   <Link className="text-link" to="/login">
                     LOGIN / SIGNUP
@@ -22,12 +21,8 @@ export const Menu = () => {
                 </div>
               </div>
             </div>
-            <div className="icons">
-              <img src={faHeart} alt="favorite" />
-            </div>
-            <div className="icons">
-              <img src={faShoppingCart} alt="shoppingcart" />
-            </div>
+            <img className="icons" src={faHeart} alt="favorite" />
+            <img className="icons" src={faShoppingCart} alt="shoppingcart" />
           </div>
           <div className="navbar">
             <div className="dropdown">
@@ -70,78 +65,78 @@ export const Menu = () => {
               CONTACT US
             </button>
           </div>
-        </BrowserRouter>
-      </header>
-    </menu>
+        </header>
+      </menu>
+    </BrowserRouter>
   );
 };
-export const MenuLogin = () => {
-  return (
-    <menu>
-      <header>
-        <BrowserRouter>
-          <div className="logo_container">
-            <div className="user-icon">
-              <div className="dropdown">
-                <img src={faUser} alt="login" />
+// export const MenuLogin = () => {
+//   return (
+//     <menu>
+//       <header>
+//         <BrowserRouter>
+//           <div className="logo_container">
+//             <div className="user-icon">
+//               <div className="dropdown">
+//                 <img src={faUser} alt="login" />
 
-                <div id="login" className="dropdown-content">
-                  <Link className="text-link" to="/login">
-                    LOGOUT
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="icons">
-              <img src={faHeart} alt="favorite" />
-            </div>
-            <div className="icons">
-              <img src={faShoppingCart} alt="shoppingcart" />
-            </div>
-          </div>
-          <div className="navbar">
-            <div className="dropdown">
-              <button type="submit" className="dropbtn">
-                CATEGORIES
-              </button>
+//                 <div id="login" className="dropdown-content">
+//                   <Link className="text-link" to="/login">
+//                     LOGOUT
+//                   </Link>
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="icons">
+//               <img src={faHeart} alt="favorite" />
+//             </div>
+//             <div className="icons">
+//               <img src={faShoppingCart} alt="shoppingcart" />
+//             </div>
+//           </div>
+//           <div className="navbar">
+//             <div className="dropdown">
+//               <button type="submit" className="dropbtn">
+//                 CATEGORIES
+//               </button>
 
-              <div className="dropdown-content">
-                <ul>
-                  <li>
-                    <Link className="text-link" to="/furniture">
-                      FURNITURE
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="text-link" to="/lamps">
-                      LAMPS
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="text-link" to="/homedecor">
-                      HOME DECOR
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="text-link" to="/linen">
-                      LINEN
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <button type="submit" className="dropbtn">
-              MONTHLY ARIVALS
-            </button>
-            <button type="submit" className="dropbtn">
-              ABOUT
-            </button>
-            <button type="submit" className="dropbtn">
-              CONTACT US
-            </button>
-          </div>
-        </BrowserRouter>
-      </header>
-    </menu>
-  );
-};
+//               <div className="dropdown-content">
+//                 <ul>
+//                   <li>
+//                     <Link className="text-link" to="/furniture">
+//                       FURNITURE
+//                     </Link>
+//                   </li>
+//                   <li>
+//                     <Link className="text-link" to="/lamps">
+//                       LAMPS
+//                     </Link>
+//                   </li>
+//                   <li>
+//                     <Link className="text-link" to="/homedecor">
+//                       HOME DECOR
+//                     </Link>
+//                   </li>
+//                   <li>
+//                     <Link className="text-link" to="/linen">
+//                       LINEN
+//                     </Link>
+//                   </li>
+//                 </ul>
+//               </div>
+//             </div>
+//             <button type="submit" className="dropbtn">
+//               MONTHLY ARIVALS
+//             </button>
+//             <button type="submit" className="dropbtn">
+//               ABOUT
+//             </button>
+//             <button type="submit" className="dropbtn">
+//               CONTACT US
+//             </button>
+//           </div>
+//         </BrowserRouter>
+//       </header>
+//     </menu>
+//   );
+// };
