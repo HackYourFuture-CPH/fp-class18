88b8dae494b6f03eb6bb823bf6de1776f10f59af
saru <saru.gmesp@gmail.com>
2021-11-14 21:02:23 +0000

    Changes done

--- src/server/api/routes/api-router.js
+++ src/server/api/routes/api-router.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 // Router imports
 const modulesRouter = require('./modules.router');
-
+const usersRouter = require('./users.router');
+const productsRouter = require('./products.router');
 const ordersRouter = require('./orders.router');
 
 const swaggerJsDoc = require('swagger-jsdoc');
@@ -32,7 +33,8 @@ router.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Application routes
 router.use('/modules', modulesRouter);
-
+router.use('/users', usersRouter);
+router.use('/products', productsRouter);
 router.use('/orders', ordersRouter);
 
 module.exports = router;
