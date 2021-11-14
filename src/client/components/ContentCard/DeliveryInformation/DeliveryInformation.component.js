23764145fd7a945820994b63b105b3703847ef7c
Yasser <Yasser.H@outlook.dk>
2021-11-14 12:54:35 +0100

    Directory rename

---
+++ src/client/components/ContentCard/DeliveryInformation/DeliveryInformation.component.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './DeliveryInformation.styles.css';
+
+export function DeliveryInformation({ sampleAddress }) {
+  return (
+    <div className="DeliveryInfo">
+      <label className="addressTitle">DELIVERY INFO:</label>
+      <ul>
+        <li>
+          <label className="addressLabel">Address:</label>
+          <label className="addressData"> {sampleAddress.address}</label>
+        </li>
+        <li>
+          <label className="zipCodeLabel">Zip code:</label>
+          <label className="zipCodeData"> {sampleAddress.zipCode}</label>
+        </li>
+        <li>
+          <label className="cityLabel">City:</label>
+          <label className="cityData"> {sampleAddress.city}</label>
+        </li>
+        <li>
+          <label className="countryLabel">Country:</label>
+          <label className="countryData"> {sampleAddress.country}</label>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+DeliveryInformation.defaultProps = {
+  editMode: false,
+  sampleAddress: {
+    address: 'Enghavevej 80',
+    city: 'København',
+    zipCode: '2450',
+    country: 'Denmark',
+  },
+};
---
+++ src/client/components/ContentCard/DeliveryInformation/DeliveryInformation.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { DeliveryInformation } from './DeliveryInformation.component.js';
+
+export default { title: 'Components / Content Card' };
+
+export const Component = () => <DeliveryInformation />;
