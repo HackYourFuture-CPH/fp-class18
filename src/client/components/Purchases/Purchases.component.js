fdc53ffc707d9c6ac7595a205fc0988d14001be2
saru <saru.gmesp@gmail.com>
2021-11-21 20:46:06 +0100

    Purchases component created

---
+++ src/client/components/Purchases/Purchases.component.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Purchases.styles.css';
+
+export default function Purchases({ orderId, date }) {
+  return (
+    <div className="infoBox">
+      <div className="wrapper">
+        <form className="form">
+          <label className="title">PURCHASES:</label>
+          <label className="orderid">ORDER ID: {orderId}</label>
+          <label className="date">DATE: {date}</label>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+Purchases.defaultProps = {
+  order: {
+    orderId: '',
+    date: '',
+  },
+};
+
+Purchases.propTypes = {
+  orderid: PropTypes.number.isRequired,
+  date: PropTypes.number.isRequired,
+};
