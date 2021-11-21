b72062c83f4848fd783a0e4600ba0ab0af185a45
saru <saru.gmesp@gmail.com>
2021-11-21 19:41:50 +0100

    Feedback Changes

--- src/server/api/controllers/orders.controller.js
+++ src/server/api/controllers/orders.controller.js
@@ -7,13 +7,16 @@ const getOrders = async () => {
 
 const getOrderById = async (id) => {
   if (!id) {
-    throw new HttpError('Id should be a number', 400);
+    throw new HttpError(
+      'Bad request. Order ID must be an integer and larger than 0',
+      400,
+    );
   }
 
   try {
     const orders = await knex('orders').select('orders').where({ id });
     if (orders.length === 0) {
-      throw new Error(`incorrect entry with the id of ${id}`, 404);
+      throw new Error(` order with the specified ID was not found`, 404);
     }
     return orders;
   } catch (error) {
