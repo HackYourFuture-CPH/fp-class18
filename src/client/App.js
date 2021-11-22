660b34d13f1b260ff7523455f5ba8f19f08a3a0b
younes <dev.meliani@gmail.com>
2021-11-23 00:07:45 +0100

    cleanup auth logic (merge useAuthentication & FirebaseContext states) add Non Auth Guard to sign-in, sign-up and reset-password

--- src/client/App.js
+++ src/client/App.js
@@ -1,36 +1,49 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import SignIn from './containers/SignIn';
 import SignUp from './containers/SignUp';
 import ResetPassword from './containers/ResetPassword';
 import AuthenticatedRoute from './components/Auth/AuthenticatedRoute';
-import { useAuthentication } from './hooks/useAuthentication';
+import NonAuthenticatedRoute from './components/Auth/NonAuthenticatedRoute.js';
 import Profile from './containers/Profile';
 import Loader from './components/Loader';
 import LandingPageContainer from './containers/LandingPage/LandingPage.Container';
 import { Menu } from './components/menu/Menu.component';
 import { Footer } from './components/Footer/Footer.component';
+import reactRouterHistory from './router-history';
+import { useFirebase } from './firebase';
 
 function App() {
-  const { isLoading } = useAuthentication();
+  const { isLoading } = useFirebase();
 
   if (isLoading) {
     return <Loader />;
   }
 
   return (
-    <Router>
+    <Router history={reactRouterHistory}>
       <Menu isAuthenticated={false} />
       <Switch>
         <Route exact path="/">
           <LandingPageContainer />
         </Route>
 
-        {/* Anonymous pages */}
-        <SignIn exact path="/sign-in" />
-        <SignUp exact path="/sign-up" />
-        <ResetPassword exact path="/reset-password" />
+        {/*
+         * All routes below are only shown when you are not authenticated - if the
+         * user is logged in, if a user is logged in, they can't see the login page
+         */}
+        <NonAuthenticatedRoute exact path="/sign-in">
+          <SignIn />
+        </NonAuthenticatedRoute>
+
+        <NonAuthenticatedRoute exact path="/sign-up">
+          <SignUp />
+        </NonAuthenticatedRoute>
+
+        <NonAuthenticatedRoute exact path="/reset-password">
+          <ResetPassword />
+        </NonAuthenticatedRoute>
 
         {/* All routes below are authenticated routes - a user must login first */}
         <AuthenticatedRoute exact path="/profile">
