f35467e109509415b0b9c12f09bc764b1221bb31
jaimakena <jaimakena@gmail.com>
2021-11-23 18:58:38 +0100

    Review commit changes

--- src/client/components/ContactForm/ContactForm.component.js
+++ src/client/components/ContactForm/ContactForm.component.js
@@ -2,33 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ContactForm.styles.css';
 
-export default function ContactForm({user}) {
-    return (
-        <div className="infoBox">
-            <div className="wrapper">
-                <form className="form">
-                    <label className="title">CONTACT:</label>
-                    <label className="fullName">Full-name:  {
-                        user.fullName
-                    }</label>
-                    <label className="email">e-mail address:  {
-                        user.email
-                    }</label>
-                </form>
-            </div>
-        </div>
-    );
+export default function ContactForm({ fullName, email }) {
+  return (
+    <div className="infoBox">
+      <div className="wrapper">
+        <form className="form">
+          <label className="title">CONTACT:</label>
+          {fullName && (
+            <label className="fullName">Full-name: {fullName}</label>
+          )}
+          {email && <label className="email">e-mail address: {email}</label>}
+        </form>
+      </div>
+    </div>
+  );
 }
 
-ContactForm.defaultProps = {
-    user: {
-        fullName: "",
-        email: ""
-    }
-};
-
 ContactForm.propTypes = {
-    user: PropTypes.shape(
-        {fullName: PropTypes.string, email: PropTypes.string}
-    )
+  fullName: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
 };
--- src/client/components/ContactForm/ContactForm.stories.js
+++ src/client/components/ContactForm/ContactForm.stories.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ContactForm from './ContactForm.component';
 
-export default {title: 'Components / ContactForm', component: ContactForm};
+export default { title: 'Components / ContactForm', component: ContactForm };
 
 // eslint-disable-next-line react/jsx-props-no-spreading
-const Template = (args) => <ContactForm {...args}/>;
-
+const Template = (args) => <ContactForm {...args} />;
 
 export const Info = Template.bind({});
 
 Info.args = {
-    user: {
-        fullName: "Jon Doe",
-        email: "jdoe@gmail.com"
-    }
+  fullName: 'Jon Doe',
+  email: 'jdoe@gmail.com',
 };
-
