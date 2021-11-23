fa48946145becb3ad83e49be5e2dffb96f7ff252
zmzade <zmahmoodzadeh@yahoo.com>
2021-11-23 19:04:05 +0100

    edit

--- src/client/containers/OrderPage/OrderPage.Container.js
+++ src/client/containers/OrderPage/OrderPage.Container.js
@@ -1,15 +1,21 @@
 import React from 'react';
-// import ContactForm from '../..components/ContactForm/ContactForm.component';
-// import DeliveryInformation from '../..components/ContentCard/DeliveryInformation/DeliveryInformation.component';
-import './OrderPage.Style.css';
+import ShoppingItem from '../..components/ShoppingItem/ShoppingItem.component';
+import ContactForm from '../..components/ContactForm/ContactForm.component';
+import DeliveryInformation from '../..components/ContentCard/DeliveryInformation/DeliveryInformation.component';
 import { useParams } from 'react-router-dom';
 import { useFetchApi } from '../../hooks/UseFetchApi';
+import './OrderPage.Style.css';
 
 const OrderPageContainer = () => {
+  const [purchase, setPurchase] = React.useState([]);
+  const [user, setUser] = React.useState([]);
   const { id } = useParams();
-  const order = useFetchApi(`orders/${id}`);
-  console.log(order.data);
-
+  const newPurchase = useFetchApi(`orders/${id}`);
+  const userInfo = useFetchApi(`users/${purchase.user_id}`);
+  console.log(newPurchase.data[0]);
+  console.log(userInfo.data[0]);
+  setPurchase(newPurchase.data[0]);
+  setUser(userInfo);
   return (
     <div>
       <h1>Order page Container</h1>
@@ -17,19 +23,39 @@ const OrderPageContainer = () => {
         <div className="order-product">
           <div className="order">
             <div>ORDER ID: {id}</div>
-            <div>ORDER STATUS: {order.orderStatus}</div>
+            <div>ORDER STATUS: {purchase.orderStatus}</div>
           </div>
           <div className="product">
             <div className="picture-name-quantity">
-              <div>ProductItem</div>
+              <div>
+                {purchase.map((product) => (
+                  <li>
+                    <ShoppingItem
+                      name={product.name}
+                      image={product.picture}
+                      quantity={product.stock_amount}
+                      price={product.price}
+                    />
+                  </li>
+                ))}{' '}
+              </div>
             </div>
           </div>
         </div>
-        <div className="total">total</div>
+        <div className="total">total Component</div>
       </div>
       <div className="delivery-contact">
-        <div className="delivery">Delivery Info</div>
-        <div className="contact">Contact:</div>
+        <div className="delivery">
+          <DeliveryInformation
+            address={user.address}
+            zipcode={user.zipcode}
+            city={user.city}
+            country={user.country}
+          />{' '}
+        </div>
+        <div className="contact">
+          <ContactForm fullName={user.full_name} email={user.email} />{' '}
+        </div>
       </div>
     </div>
   );
