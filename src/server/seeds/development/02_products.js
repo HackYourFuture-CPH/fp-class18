16ebe3d37026a03bae75b0757e5f750b3f309f03
simonreddy2001 <simon.reddy2001@gmail.com>
2021-11-25 06:37:27 +0100

    procfile update check

--- src/server/seeds/development/02_products.js
+++ src/server/seeds/development/02_products.js
@@ -5,7 +5,7 @@ exports.seed = function (knex) {
       return knex('products').insert([
         {
           id: 1,
-          name: 'Ceramic pot',
+          name: 'Ceramic pot simon',
           price: 50,
           color: 'Green',
           size: 'Small',
