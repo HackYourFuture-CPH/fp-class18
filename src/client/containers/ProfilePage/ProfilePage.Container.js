d92aaffa9ab4d3f8a1de76d601f3041033a96215
saru <saru.gmesp@gmail.com>
2021-11-27 11:11:05 +0100

    Profile-page

--- src/client/containers/ProfilePage/ProfilePage.Container.js
+++ src/client/containers/ProfilePage/ProfilePage.Container.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { ContactForm } from '../..components/ContactForm/ContactForm.component.js';
+import DeliveryInformation from '../..components/ContentCard/DeliveryInformation/DeliveryInformation.component';
+import { useParams } from 'react-router-dom';
+import { useFetchApi } from '../../hooks/UseFetchApi';
 import './ProfilePage.Style.css';
 
 const ProfilePageContainer = () => {
+  const [user, setUser] = React.useState([]);
+  const userInfo = useFetchApi(`users/${purchase.user_id}`);
+  setUser(userInfo);
+  console.log(userInfo.data[0]);
   return (
     <div>
       <h1>Profile page Container</h1>
+      <div className="delivery-contact">
+        <div className="delivery">
+          <DeliveryInformation
+            address={user.address}
+            zipcode={user.zipcode}
+            city={user.city}
+            country={user.country}
+          />{' '}
+        </div>
+        <div className="contact">
+          <ContactForm fullName={user.full_name} email={user.email} />{' '}
+        </div>
+        <div className="purchases">
+          <Purchases />{' '}
+        </div>
+      </div>
     </div>
   );
 };
