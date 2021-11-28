1ec01e5c9fe85def329094fee68c8fb9eac3337d
jul-s <jul.samorodova@gmail.com>
2021-11-28 13:47:33 +0100

    Added ProfilePage App.js so we don	 have error

--- src/client/App.js
+++ src/client/App.js
@@ -6,6 +6,7 @@ import SignUp from './containers/SignUp';
 import ResetPassword from './containers/ResetPassword';
 import AuthenticatedRoute from './components/Auth/AuthenticatedRoute';
 import NonAuthenticatedRoute from './components/Auth/NonAuthenticatedRoute.js';
+import ProfilePage from './containers/ProfilePage/ProfilePage.Container';
 import Loader from './components/Loader';
 import LandingPageContainer from './containers/LandingPage/LandingPage.Container';
 import { Menu } from './components/menu/Menu.component';
@@ -57,7 +58,9 @@ function App() {
         </NonAuthenticatedRoute>
 
         {/* All routes below are authenticated routes - a user must login first */}
-        <AuthenticatedRoute exact path="/profile"></AuthenticatedRoute>
+        <AuthenticatedRoute exact path="/profile">
+          <ProfilePage />
+        </AuthenticatedRoute>
       </Switch>
       <Footer />
     </Router>
