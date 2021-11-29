065c1512cba226f0d4b9f0933651c088bb86f57d
simonreddy2001 <simon.reddy2001@gmail.com>
2021-11-29 09:57:50 +0100

    icon

--- src/client/components/FloatButton/FloatButton.component.js
+++ src/client/components/FloatButton/FloatButton.component.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './FloatButton.styles.css';
-import { FaHome } from 'react-icons/fa';
+import homeIcon from '../../assets/images/homeicon.png';
 
 export default function FloatButtonComponent() {
   return (
     <div>
       <a href="/" className="float">
-        <FaHome className="my-float" />
+        <img src={homeIcon} alt="home-icon" />
       </a>
       <div className="label-container">
         <div className="label-text">Go to Home</div>
-        <i className="fa fa-play label-arrow" />
       </div>
     </div>
   );
