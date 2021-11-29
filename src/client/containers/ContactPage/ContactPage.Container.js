cf214ca2c03a614ea2e60cac0515f3b64e4677d5
simonreddy2001 <simon.reddy2001@gmail.com>
2021-11-29 10:18:15 +0100

    uninstall package

--- src/client/containers/ContactPage/ContactPage.Container.js
+++ src/client/containers/ContactPage/ContactPage.Container.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import './ContactPage.Style.css';
 import contactUs from '../../assets/images/contactUs.png';
-import {
-  FaMapMarkerAlt,
-  FaEnvelope,
-  FaPhoneSquare,
-  FaClock,
-} from 'react-icons/fa';
+import map from '../../assets/images/location-icon.png';
+import envelop from '../../assets/images/email-icon.png';
+import phone from '../../assets/images/Phone_icon.png';
+import clock from '../../assets/images/clock-icon.png';
 
 const ContactpageContainer = () => {
   return (
     <div className="ContactpageContainer">
       <img src={contactUs} alt="contact-us" />
       <div>
-        <FaMapMarkerAlt className="item" />
+        <img src={map} alt="location" className="item" />
         <h2>Copenhagen, Denmark</h2>
       </div>
       <div>
-        <FaPhoneSquare className="item" />
+        <img src={phone} alt="phoneNumber" className="item" />
         <h2>+45 000 00000</h2>
       </div>
       <div>
-        <FaEnvelope className="item" />
+        <img src={envelop} alt="email" className="item" />
         <h2>email@address.com</h2>
       </div>
       <div>
-        <FaClock className="item" />
+        <img src={clock} alt="openingTimes" className="item" />
         <h2>Mon - Fri 8:00 AM to 6:00 PM</h2>
       </div>
     </div>
