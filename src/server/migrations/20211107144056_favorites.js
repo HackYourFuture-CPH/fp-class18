0f1b6860218174b5c04c467a31da68c4c7f8b0d3
jul-s <jul.samorodova@gmail.com>
2021-11-29 17:14:18 +0100

    Added changes to favorite table to fix build error

--- src/server/migrations/20211107144056_favorites.js
+++ src/server/migrations/20211107144056_favorites.js
@@ -1,12 +1,13 @@
 exports.up = function (knex) {
   return knex.schema.createTable('favorites', (table) => {
-    table.string('user_id').notNullable().references('users.id');
+    table.string('user_id').notNullable();
     table
       .integer('product_id')
       .notNullable()
       .unsigned()
       .references('products.id');
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+    table.foreign('user_id').references('users.id');
   });
 };
 
