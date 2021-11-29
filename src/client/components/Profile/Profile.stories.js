85e84f76901e2fc51b9c63fd2676f686b61989f4
saru <saru.gmesp@gmail.com>
2021-11-29 21:54:45 +0100

    Profile component

---
+++ src/client/components/Profile/Profile.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Profile } from './Profile.component';
+
+export default { title: 'Components / Profile', component: Profile };
+
+export const Info = () => <Profile />;
