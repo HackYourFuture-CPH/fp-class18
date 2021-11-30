0a2a866e3ca426271144f76d1ba1a890266307bd
simonreddy2001 <simon.reddy2001@gmail.com>
2021-11-30 18:28:41 +0100

    feedback

--- src/client/components/FloatButton/FloatButton.component.js
+++ src/client/components/FloatButton/FloatButton.component.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './FloatButton.styles.css';
-import homeIcon from '../../assets/images/homeicon.png';
 
 export default function FloatButtonComponent() {
   return (
     <div>
       <a href="/" className="float">
-        <img src={homeIcon} alt="home-icon" />
+        <p>Back</p>
       </a>
       <div className="label-container">
         <div className="label-text">Go to Home</div>
