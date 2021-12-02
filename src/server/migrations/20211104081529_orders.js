369c2adcb0413491ca20ad321ac7045a769c4941
BikramRawat <mail2bikram44@gmail.com>
2021-12-02 19:02:41 +0100

    Changed user_id to string from integer

--- src/server/migrations/20211104081529_orders.js
+++ src/server/migrations/20211104081529_orders.js
@@ -6,7 +6,7 @@ exports.up = function (knex) {
       .defaultTo('created')
       .notNullable();
     table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
-    table.integer('user_id').unsigned().notNullable();
+    table.string('user_id').unsigned().notNullable();
     table.foreign('user_id').references('users.id');
   });
 };
