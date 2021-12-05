93b473f4ab975f90207e64e0e5dcde8feb568da2
simonreddy2001 <simon.reddy2001@gmail.com>
2021-12-05 20:40:29 +0100

    contact page change and product name

--- src/client/containers/ContactPage/ContactPage.Container.js
+++ src/client/containers/ContactPage/ContactPage.Container.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import './ContactPage.Style.css';
-import contactUs from '../../assets/images/contactUs.png';
-import map from '../../assets/images/location-icon.png';
-import envelop from '../../assets/images/email-icon.png';
-import phone from '../../assets/images/Phone_icon.png';
-import clock from '../../assets/images/clock-icon.png';
+import contactUs from '../../assets/images/contact-us.png';
 
 const ContactpageContainer = () => {
   return (
     <div className="ContactpageContainer">
       <img src={contactUs} alt="contact-us" />
-      <div>
-        <img src={map} alt="location" className="item" />
+      <div className="boarder">
+        <h1>LOCATION</h1>
         <h2>Copenhagen, Denmark</h2>
       </div>
-      <div>
-        <img src={phone} alt="phoneNumber" className="item" />
+      <div className="boarder">
+        <h1>PHONE NUMBER</h1>
         <h2>+45 000 00000</h2>
       </div>
-      <div>
-        <img src={envelop} alt="email" className="item" />
+      <div className="boarder">
+        <h1>EMAIL</h1>
         <h2>email@address.com</h2>
       </div>
-      <div>
-        <img src={clock} alt="openingTimes" className="item" />
+      <div className="boarder">
+        <h1>TIMINGS</h1>
         <h2>Mon - Fri 8:00 AM to 6:00 PM</h2>
       </div>
     </div>
