e77b3fc25974349b82469b27a0edd36bf1cc3c53
varsha verma <varsha.verma85@gmail.com>
2021-12-08 01:41:38 +0100

    fix css and delivery info and contact based on auth or guest

--- src/client/containers/CartPage/CartPage.Container.js
+++ src/client/containers/CartPage/CartPage.Container.js
@@ -10,11 +10,15 @@ import { useFirebase } from '../../firebase/FirebaseContext';
 import { PropTypes } from 'prop-types';
 import { useShoppingCartContext } from '../../context/shoppingCart/shoppingCartContext';
 import { useHistory } from 'react-router-dom';
+import DeliveryInfoV2 from '../../components/DeliveryInfo/DeliveryInfoV2.component';
+import { useFetchApi } from '../../hooks/UseFetchApi';
 
 const CartPageContainer = ({ isAuthenticated }) => {
   const [cartItem, setCartItem] = React.useState([]);
   const [IsLoading, setIsLoading] = React.useState(true);
   const [total, setTotal] = React.useState(0);
+
+  const [user, setUser] = React.useState({});
   const { auth } = useFirebase();
 
   const { shoppingCart, changeProductQuantity } = useShoppingCartContext();
@@ -66,54 +70,88 @@ const CartPageContainer = ({ isAuthenticated }) => {
     const productId = cartItem[index].productId;
     changeProductQuantity(productId, 0);
   };
+
+  const userId = (isAuthenticated && auth.currentUser.uid) || '';
+
+  const userInfo = useFetchApi(`users/${userId}`);
+
+  React.useEffect(() => {
+    if (!userInfo.isLoading) {
+      setUser(userInfo.data[0]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userInfo]);
+
+  const showCartItems = () => {
+    if (cartItem.length === 0) {
+      return <h1> Your Cart is Empty </h1>;
+    }
+
+    return cartItem.map((item, index) => {
+      return (
+        <ShoppingItem
+          productName={item.name}
+          quantity={item.stock_amount}
+          price={item.price}
+          productImg={item.picture}
+          initValue={item.quantity}
+          getQuantity={(value) => {
+            getItemQuantity(index, value);
+          }}
+          onDelete={() => handleOnDeleteItem(index)}
+        />
+      );
+    });
+  };
+  const showDeliveryInfo = () => {
+    if (userId) {
+      return <DeliveryInfoV2 editMode={true} vertDisplay={false} user={user} />;
+    }
+  };
+  const showContactDetails = () => {
+    if (userId) {
+      return (
+        <ContactForm
+          fullName={auth.currentUser.displayName}
+          email={auth.currentUser.email}
+        />
+      );
+    }
+  };
+
   return (
-    <div>
-      <h1>SHOPPING CART</h1>
-      <div className="main">
-        <div className="left">
-          <div className="item-list">
-            {IsLoading ? (
-              <Loader />
-            ) : (
-              cartItem.map((item, index) => {
-                return (
-                  <ShoppingItem
-                    productName={item.name}
-                    quantity={item.stock_amount}
-                    price={item.price}
-                    productImg={item.picture}
-                    initValue={item.quantity}
-                    getQuantity={(value) => {
-                      getItemQuantity(index, value);
-                    }}
-                    onDelete={() => handleOnDeleteItem(index)}
-                  />
-                );
-              })
-            )}
-          </div>
-        </div>
-        <div className="right">
-          <div className="total-price">
-            <TotalPrice subTotal={total} />
-          </div>
-          <div className="contact">
-            <ContactForm
-              fullName={
-                isAuthenticated ? `${auth.currentUser.displayName}` : 'Guest'
-              }
-              email={isAuthenticated ? `${auth.currentUser.email}` : ''}
-            />
-          </div>
-          <div>
-            <div className="review-btn">
-              <ButtonComponent title="REVIEW ORDER" />
+    <div className="cart-page">
+      <div className="header">
+        <h1>SHOPPING CART</h1>
+      </div>
+      <div className="cart-page-containter">
+        <div className="top">
+          <div className="left">
+            <div className="cart-items">
+              {IsLoading ? <Loader /> : showCartItems()}
             </div>
-            <div className="shopping-btn">
-              <ButtonComponent
-                title="KEEP SHOPPING"
-                onClick={() => history.push('/')}
-              />
+            <div className="delivery-info">
+              {userId && userInfo.isLoading ? <Loader /> : showDeliveryInfo()}
+            </div>
+          </div>
+          <div className="right">
+            <div className="total">
+              <TotalPrice subTotal={total} />
+            </div>
+            <div className="contact">
+              {userId && userInfo.isLoading ? <Loader /> : showContactDetails()}
+            </div>
+            <div className="buttons">
+              <div className="review-btn">
+                <ButtonComponent title="REVIEW ORDER" />
+              </div>
+              <div className="shopping-btn">
+                <ButtonComponent
+                  title="KEEP SHOPPING"
+                  backgroundColor="#fff"
+                  onClick={() => history.push('/')}
+                />
+              </div>
             </div>
           </div>
         </div>
